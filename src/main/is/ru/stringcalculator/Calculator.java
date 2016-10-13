2829e9e40b5823bd34a39f59e4ce0a8f27a5a1ba
Sverrir V <sverrirvh@gmail.com>
2016-10-13 15:59:15 +0000

    More than 2 numbers works

--- src/main/is/ru/stringcalculator/Calculator.java
+++ src/main/is/ru/stringcalculator/Calculator.java
@@ -1,23 +1,32 @@
 package is.ru.stringcalculator;
 
-public class Calculator{
-  public static int add(String text)
-  {
-    if(text.equals(""))
-      return 0;
-    else if(text.contains(",")){
-      String [] numbers = text.split(",");
-      return toInt(numbers[0]) + toInt(numbers[1]);
-    }
-    else
-      return 1;
+public class Calculator {
+	public static int add(String text){
+    	if(text.equals("")){
+    		return 0;
+    	}
+    	else if(text.contains(",")){
+    		return sum(splitNumbers(text));
+    	}
+    	else{
+    		return 1;
+    	}
+  	}
 
-  }
+  	private static int toInt(String number)
+  	{
+  		return Integer.parseInt(number);
+  	}
 
-  private static int toInt(String text){
-    return Integer.parseInt(text);
-  }
+  	private static String[] splitNumbers(String numbers){
+  		return numbers.split(",");
+  	}
 
-
-
+  	private static int sum(String[] numbers){
+  		int total = 0;
+  		for(String number : numbers){
+  			total += toInt(number);
+  		}
+  		return total;
+  	}
 }
