8cffd1926052729baaec55f208d11431a1f97c38
Sverrir V <sverrirvh@gmail.com>
2016-10-14 17:31:03 +0000

    Throw minus if number is minus

--- src/main/is/ru/stringcalculator/Calculator.java
+++ src/main/is/ru/stringcalculator/Calculator.java
@@ -1,32 +1,79 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
-	public static int add(String text){
-    	if(text.equals("")){
+	public static int add(String text)
+	{
+    	if(text.equals(""))
+			{
     		return 0;
     	}
-    	else if(text.contains(",")){
-    		return sum(splitNumbers(text));
+    	else if(text.contains(","))
+			{
+          if(text.contains("-"))
+            {
+							int[] numbs = getNegatives(text);
+    					String str = "";
+    					for(int num : numbs){
+    							str = str + num + ",";
+    					}
+
+    					throw new IllegalArgumentException("Negatives not allowed: " + str);
+            }
+        return sum(splitNumbers(text));
     	}
     	else{
     		return 1;
     	}
   	}
+//Add ends helpers begin.
+		//Minus numbers not allowed.
+		private static int[] getNegatives(String text)
+		{
+		  		String[] numbers = splitNumbers(text);
+		  		int size = 0;
+		  		for(String numb : numbers)
+					{
+		  			if(toInt(numb) < 0)
+						{
+		  				size++;
+		  			}
+		  		}
+		  		int[] results = new int[size];
+		  		int counter = 0;
+		  		for(String number : numbers)
+					{
+		  			if(toInt(number) < 0)
+						{
+		  				results[counter] = toInt(number);
+		  				counter++;
+		  			}
+		  		}
+		  		return results;
+		}
 
+		//Cast to int.
   	private static int toInt(String number)
   	{
   		return Integer.parseInt(number);
   	}
 
-  	private static String[] splitNumbers(String numbers){
+		//Split numbers by ",".
+  	private static String[] splitNumbers(String numbers)
+		{
   		return numbers.split(",");
   	}
+		//check if nuumber is greather than 1000.
 
-  	private static int sum(String[] numbers){
+
+		//Sum numbers
+  	private static int sum(String[] numbers)
+		{
   		int total = 0;
-  		for(String number : numbers){
-  			total += toInt(number);
+  		for(String number : numbers)
+			{
+  				total += toInt(number);				
   		}
   		return total;
   	}
+
 }
--- src/test/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/is/ru/stringcalculator/CalculatorTest.java
@@ -15,17 +15,21 @@ public class CalculatorTest{
     assertEquals(1,Calculator.add("1"));
   }
 
+  @Test
+  public void testTwoNumbers(){
+    assertEquals(3,Calculator.add("1,2"));
+  }
 
-    @Test
-    public void testTwoNumbers(){
-      assertEquals(3,Calculator.add("1,2"));
+  @Test
+  public void testXNumbers(){
+    assertEquals(6,Calculator.add("1,2,3"));
+  }
 
-    }
-
-    @Test
-    public void testXNumbers(){
-      assertEquals(6,Calculator.add("1,2,3"));
-
-    }
+  @Test
+  (expected = IllegalArgumentException.class)
+  public void testnoMinusNum(){
+    assertEquals(1, Calculator.add("-1,2"));
+    assertEquals(-4, Calculator.add("2,-4,3,-5"));
+  }
 
 }
